08617100fd711d2bc45b91c1a78e96c8ed925dd8
Ramcharan <ramcharan.gaddam@gmail.com>
2020-08-10 16:53:47 +0530

    Initial Commit

---
+++ Traffic light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Traffic light
+//
+//  Created by Ramcharan Reddy Gaddam on 10/08/20.
+//  Copyright © 2020 Ramcharan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
